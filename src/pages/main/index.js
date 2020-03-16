66bb18ffa4695de7409466c288a1f348e14957ad
gustavo <gu_kiub@hotmail.com>
2020-03-16 09:27:41 -0300

    projeto-finalizado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,63 +8,33 @@ import './styles.css';
 
 export default class Main extends Component {
     state = {
-        products: [],
-        productInfo: {},
-        page: 1
+        pokemon: [],
     }
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async (page = 1) => {
-        const response = await api.get(`/products?page=${page}`);
+    loadProducts = async () => {
+        const response = await api.get('pokemon/');
 
-        const { docs, ...productInfo } = response.data;
+        this.setState({ pokemon: response.data.results });
 
-        this.setState({
-            products: docs, productInfo, page
-        });
+        console.log(response.data.results);
     };
     
-    prevPage = () => {
-        const { page, productInfo } = this.state;
-
-        if(page === 1) return;
-
-        const pageNumber = page - 1;
-        
-        this.loadProducts(pageNumber)
-    }
-
-    nextPage = () => {
-        const { page, productInfo } = this.state;
-
-        if(page === productInfo.pages) return;
-        
-        const pageNumber = page + 1;
-
-        this.loadProducts(pageNumber);
-    }
-
     render() {
-        const { products, page, productInfo } = this.state;
+        const {pokemon} = this.state;
 
         return (
             <div className="product-list">
-                {this.state.products.map(product => (
-                    <article key={product._id}>
-                        <strong>{product.title}</strong>
-                        <p>{product.description}</p>
+                {pokemon.map(product => (
+                    <article key={product.id}>
+                        <strong>{product.name}</strong>
+                        <p>{product.url}</p>
 
-                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
-
-                <div className="actions">
-                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
-                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Proximo</button>
-                </div>
             </div>
         )
     }
